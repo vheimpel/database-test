15f03a167239988603c26ee5f9440f97baa605f5
vheimpel <vheimpel@gmail.com>
2017-05-24 01:09:14 +0000

    Knex file fully functional

--- lookup_people_knex.js
+++ lookup_people_knex.js
@@ -9,21 +9,32 @@ var knexConfig = {
     "ssl": true
   }
 }
+
 var knex = require('knex')(knexConfig)
 
 var someId = process.argv[2];
+
+//Function to loop through array
+let printLine = (row) => {
+      console.log("-"+
+        row.id,
+        row.first_name,
+        row.last_name,
+       "'"+row.birthdate.toISOString().slice(0, 10)+"'");
+    }
 
 if (!someId) { //Checks if the user has provided a name before continuing
     console.error('Empty!');
  } else {
 
 knex
-  .select('first_name').from('famous_people').where('first_name', someId)
+  .select('*').from('famous_people').where('first_name', someId)
   .then(results => {
-    if ('first_name' != someId) {
+    if (results.length === 0) {
     console.error('Not in database!');
     } else {
-      console.log(results); // Prints the results
+      // display all results received
+      results.forEach(printLine)
     }
   })
 }
